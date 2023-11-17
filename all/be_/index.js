55969b43f6dee5c11ce88c3452cd7e1fc53c0e6c
Brian Takita <brian.takita@gmail.com>
2023-11-17 04:36:06 -0500

    ctx__delete: is_source_: - default ()=>true

--- all/be_/index.js
+++ all/be_/index.js
@@ -174,19 +174,17 @@ export function ctx__delete(
 	ctx,
 	be_OR_id,
 ) {
-	const is_source_ =
-		be__is_source__(be_OR_id)
-		|| (()=>true)
+	const is_source_ = be__is_source__(be_OR_id)
 	if (Array.isArray(ctx)) {
 		for (let _ctx of ctx) {
-			if (is_source_(_ctx, ctx)) {
+			if (!is_source_ || is_source_(_ctx, ctx)) {
 				ctx__delete(_ctx, be_OR_id)
 			}
 		}
 	} else {
 		/** @type {MapCtx} */
 		let map_ctx = /** @type {any} */ctx
-		if (is_source_(map_ctx, ctx)) {
+		if (!is_source_ || is_source_(map_ctx, ctx)) {
 			let { id } = be_OR_id
 			if (id) {
 				map_ctx.delete(id)
