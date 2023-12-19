e97d637ffeda4e17294afdf3d317b4afa70ba00d
Brian Takita <brian.takita@gmail.com>
2023-12-19 03:19:50 -0500

    - major: + Ctx ns (namespace):
    be_:
    Be:
    be_o_T:
    be_config_T
    Ctx:
    generics: + ns_T
    + ns:ns_T
    - is_source_
    be__has_:
    be_map__find:
    be__val_:
    be_lock_memosig_triple_:
    be_lock_memosig_triple_T:
    be_memo_pair_:
    be_memo_pair_T:
    be_memosig_triple_:
    be_memosig_triple_T:
    be_sig_triple_:
    be_sig_triple_T:
    generics: + ns_T
    be: generics:
    + ns_T
    + ctx_T
    ctx__set:
    ctx__delete:
    generics:
    + ns_T
    + ctx_T
    arguments: + ns?:string
    ctx__new:
    ctx_:
    returns Ctx<''>
    + BeMap
    + BeMapO
    + Ctx_wide_T
    + Ctx_s_T
    + Ctx_s_wide_T
    + ctx__be_T
    + ctx__get_T
    + ctx__set_T
    + ns_ctx__new
    rmemo: bundle size optimizations: .includes instead of .indexOf
    exports: + ./test
    + Expect
    + Equal
    TupleExclude: fix: type error
    + TupleMemberExtends
    + TupleValues
    + TupleConcat
    + TupleToUnion
    - be___T
    - be__return_T
    
    size-limit:
    
    ctx_: - 1 B
    ns_ctx_: - 38 B
    be_: - 43 B
    be_ ctx_: - 40 B
    be_ ns_ctx_: - 8 B
    be_ ctx_ ns_ctx_: - 8 B
    memo_: - 4 B
    memo_ sig_: - 5 B
    memo_ sig_ be_ ctx_: - 34 B
    memo_ sig_ be_ ctx_ be_memo_pair_ be_sig_triple_: - 35 B

--- all/andand/index.d.ts
+++ all/andand/index.d.ts
@@ -4,4 +4,4 @@
 export declare function andand<Val, Out>(obj:Val, ...name_a:andand_key_T<Val>[]):Out|null
 export declare type andand_key_T<Val> = keyof Val|andand_key__T<Val>
 export declare type andand_key__T<Val> = (val:Val)=>keyof Val
-export declare type andand_key_fn_T = andand_key__T
+export declare type andand_key_fn_T<Val> = andand_key__T<Val>
