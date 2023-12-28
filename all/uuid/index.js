beb1d5347880d28982f0698271c4d04558731e95
Brian Takita <brian.takita@gmail.com>
2023-12-27 21:44:42 -0500

    ./uuid:
    	format
    	+ size-limit

--- all/uuid/index.js
+++ all/uuid/index.js
@@ -4,13 +4,13 @@ export function uuid_() {
 	for (
 		;
 		a++ < 36;
-		b += a * 51 & 52 // if "a" is not 9 or 14 or 19 or 24
-			? (a ^ 15 // if "a" is not 15
-					? 8 ^ Math.random() * (a ^ 20 ? 16 : 4 // unless "a" is 20, in which case a random number from 8 to 11
-				) : 4 //	otherwise 4
-			).toString(16) : '-' //	in other cases (if "a" is 9,14,19,24) insert "-"
-	) {
-	}
+		b += a * 51 & 52 /*if "a" is not 9 or 14 or 19 or 24*/
+			? (a ^ 15 /*if "a" is not 15*/
+				? 8 ^ Math.random() * (a ^ 20 ? 16 : 4 /*unless "a" is 20, in which case a random number from 8 to 11*/)
+				: 4 /*otherwise 4*/
+			).toString(16)
+			: '-' /*in other cases (if "a" is 9,14,19,24) insert "-"*/
+	) { /* empty */ }
 	return b
 }
 export {
